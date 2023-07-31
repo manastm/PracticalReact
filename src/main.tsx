17758c7fbf05eb388dc044c1115e0239ae470716
manastm <manastm@live.unc.edu>
2023-07-30 23:56:06 -0400

    changing the js methods

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+//import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 // import { app } from './firebase'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// )
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
